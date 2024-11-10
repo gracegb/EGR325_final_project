2bf5c491e97e3804ec6fda30d50dc7525294dc9d
Anquera <155914502+Anquera@users.noreply.github.com>
2024-11-09 17:28:31 -0800

    Database Creation + Customer table

---
+++ CreateDatabase.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Restaurant;
+
+CREATE TABLE Customer (
+	CustomerID INT NOT NULL auto_increment,
+    CustomerName varchar(100) NOT NULL,
+    CustomerEmail varchar(150) NOT NULL UNIQUE,
+    PhoneNumber varchar(15) NOT NULL UNIQUE,
+    Address varchar(225),
+    PRIMARY KEY (CustomerID)
+);
