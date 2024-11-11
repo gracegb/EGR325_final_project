e0e2382f8cf00ede4df644aaf365ee3d6e310207
gracegb <112580544+gracegb@users.noreply.github.com>
2024-11-10 21:29:19 -0800

    dummy data added

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -18,3 +18,15 @@ CREATE TABLE Reservation (
     PRIMARY KEY (ReservationID),
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON DELETE CASCADE
 );
+
+INSERT INTO Customer (CustomerName, CustomerEmail, PhoneNumber, Address) VALUES
+	('John Smith', 'johnsmith@gmail.com', '1234567890', '123 Mag Ave, Riverside, CA'),
+	('Jane Doe', 'janedoe@gmail.com', '2345678901', '123 Mag Ave, Riverside, CA'),
+	('Ronald Ellis', 'rellis@gmail.com', '4567890123', '456 Mag Ave, Riverside, CA');
+    
+INSERT INTO Reservation (CustomerID, ReservationDate, NumberOfPeople, NumberOfPeople, Status, SpecialRequests) VALUES
+	(1, '2024-11-15 18:30:00', 3, 'Confirmed', 'N/A'),
+    (2, '2024-11-15 15:00:00', 8, 'Cancelled', 'wheelchair accessible'),
+	(3, '2024-11-16 19:00:00', 2, 'Confirmed', 'birthday');
+
+    
