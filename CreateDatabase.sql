0a5fb405f18990197cd80ebdf3b80263f292f73b
Anquera <155914502+Anquera@users.noreply.github.com>
2024-11-18 13:26:01 -0800

    Update CreateDatabase.sql
    Removing insertion data to be put in dummydata file

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -95,18 +95,6 @@ CREATE TABLE Reservation (
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON DELETE CASCADE
 );
 
--- Customer dummy data
-INSERT INTO Customer (CustomerID, CustomerName, CustomerEmail, PhoneNumber, Address) VALUES
-	(4, 'John Smith', 'johnsmith@gmail.com', '1234567890', '123 Mag Ave, Riverside, CA'),
-	(6, 'Jane Doe', 'janedoe@gmail.com', '2345678901', '123 Mag Ave, Riverside, CA'),
-	(8, 'Ronald Ellis', 'rellis@gmail.com', '4567890123', '456 Mag Ave, Riverside, CA');
-
--- Reservation dummy data
-INSERT INTO Reservation (CustomerID, ReservationDate, NumberOfPeople, Status, SpecialRequests) VALUES
-	(4, '2024-11-15 18:30:00', 3, 'Confirmed', 'N/A'),
-    	(6, '2024-11-15 15:00:00', 8, 'Cancelled', 'wheelchair accessible'),
-	(8, '2024-11-16 19:00:00', 2, 'Confirmed', 'birthday');
-
 -- Create the Waitlist table
 CREATE TABLE Waitlist (
     WaitlistID INT NOT NULL AUTO_INCREMENT,
