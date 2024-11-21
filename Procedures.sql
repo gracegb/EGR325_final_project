e1023e6a276cd01c2fb35bcdcd286319dfa3a610
Anquera <155914502+Anquera@users.noreply.github.com>
2024-11-20 16:36:51 -0800

    Update Procedures.sql
    Adding Procedures

--- Procedures.sql
+++ Procedures.sql
@@ -2,16 +2,120 @@
 ----|--PROCEDURES--|----
 ------------------------
 
--- Update the price of a specific menu item
-
-CREATE PROCEDURE UpdateMenuItemPrice(
-    IN p_ItemName VARCHAR(225),
-    IN p_NewPrice VARCHAR(10,2)
+--VIEW MENU
+CREATE PROCEDURE ViewMenu (
+    IN p_MenuName VARCHAR(225)
 )
 BEGIN
-    Update MenuItem
-    SET price = p_NewPrice
-    WHERE ItemName = p_ItemName;
+    DECLARE v_MenuID;
+    
+    SELECT MenuID INTO v_MenuID
+        FROM Menu
+        WHERE MenuName = p_MenuName;
+
+    IF v_MenuID IS NOT NULL THEN
+        SELECT * FROM MenuItem             
+        WHERE MenuID = v_MenuID;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Menu does not exist';
+    END IF;
+END;
+
+--ADD A Menu
+CREATE PROCEDURE AddMenu (
+    IN p_MenuName VARCHAR(225) NOT NULL,
+    IN p_MenuDescription TEXT,
+    IN t_StartTime TIME NOT NULL,
+    IN t_EndTime TIME NOT NULL,
+)
+BEGIN
+    INSERT INTO Menu (MenuName, MenuDescription, StartTime, EndTime) VALUES
+    (p_MenuName, p_MenuDescription, t_StartTime, t_EndTime);
+END;
+
+--DELETE a Menu Item from a Menu
+CREATE PROCEDURE RemoveItemFromMenu (
+    IN p_MenuName VARCHAR(225),
+    IN p_MenuItemName VARCHAR(225)
+) 
+BEGIN
+    DECLARE v_MenuID INT;
+    MenuID INTO v_MenuID FROM MenuContent
+    WHERE MenuName = p_MenuName;
+
+    DECLARE v_MenuItemID From MenuItem
+    MenuItemID INTO v_MenuItemID FROM MenuContent
+    WHERE MenuItemName = p_MenuItemName;
+
+    DELIMITER //
+
+    START TRANSACTION;
+        IF v_MenuID IS NOT NULL AND v_MenuItemID IS NOT NULL THEN
+            DELETE * FROM MenuContent
+            WHERE MenuID = v_MenuID AND MenuItemID = v_MenuItemID;
+        ELSE
+            -- Rollback if no Matches for MenuItem found
+            ROLLBACK;
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Item is not in Menu and therefore cannot be deleted';
+        END IF;
+    END //
+    DELIMITER;
+END;
+
+--DELETE A MENU
+CREATE PROCEDURE DeleteMenu (
+    IN p_MenuName VARCHAR(225) NOT NULL,
+)
+BEGIN
+    DECLARE v_MenuID INT;
+    MenuID INTO v_MenuID FROM Menu
+    WHERE MenuName = p_MenuName;
+
+    DELIMITER //
+
+    START TRANSACTION;
+        IF v_MenuID IS NOT NULL THEN
+            DELETE * FROM Menu, MenuContent
+            WHERE MenuID = v_MenuID
+        ELSE
+            -- Rollback if no Matches for MenuItem found
+            ROLLBACK;
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Menu item does not exist';
+        END IF;
+    END //
+
+DELIMITER ;
+
+--UPDATE Menu start time and end time 
+CREATE PROCEDURE UpdateMenuTimeWindow (
+    IN p_MenuName VARCHAR(225),
+    IN t_StartTime TIME,
+    IN t_EndTime TIME
+)
+BEGIN 
+    DECLARE v_MenuID INT;
+    MenuID INTO v_MenuID FROM Menu
+    WHERE MenuName = p_MenuName;
+
+    IF v_MenuID IS NOT NULL THEN
+        UPDATE Menu
+        SET StartTime = t_StartTime
+        AND SET EndTime = t_EndTime
+        WHERE MenuID = v_MenuID;
+    ELSE
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Menu item does not exist';
+    END IF;
+
+-- ADD A MENU ITEM 
+CREATE PROCEDURE AddMenuItem (
+    IN v_MenuID,
+    IN p_ItemName VARCHAR(225) NOT NULL,
+    IN p_Price DECIMAL(10,2) NOT NULL,
+    IN p_ItemDescription TEXT
+)
+BEGIN
+    INSERT INTO MenuItem (MenuID, ItemName, Price, ItemDescription) VALUES
+    (v_MenuID, p_ItemName, p_Price, p_ItemDescription);
 END;
 
 --DELETE A MENU ITEM AND RECIPE WITH IT
@@ -49,23 +153,37 @@ END //
 
 DELIMITER ;
 
+-- Update the price of a specific menu item
 
-CREATE PROCEDURE ViewMenu (
-    IN p_MenuName VARCHAR(225)
+CREATE PROCEDURE UpdateMenuItemPrice(
+    IN p_ItemName VARCHAR(225),
+    IN p_NewPrice VARCHAR(10,2)
 )
 BEGIN
-    DECLARE v_MenuID;
-    
-    SELECT MenuID INTO v_MenuID
-        FROM Menu
-        WHERE MenuName = p_MenuName;
+    Update MenuItem
+    SET price = p_NewPrice
+    WHERE ItemName = p_ItemName;
+END;
 
-    IF v_MenuID IS NOT NULL THEN
-        SELECT * FROM MenuItem             
-            WHERE MenuID = v_MenuID;
-        ELSE
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Menu does not exist';
+
+--View the Recipe of a given MenuItem
+CREATE PROCEDURE ViewRecipe (
+    IN p_MenuItemName
+)
+BEGIN
+    DECLARE v_MenuItemID INT;
+
+    MenuItemID INTO v_MenuItemID 
+        FROM MenuItem
+        WHERE MenuItemName = p_MenuItemName;
+    
+    IF v_MenuItemID IS NOT NULL THEN
+        SELECT * FROM Ingredient
+        WHERE MenuItemID = v_MenuItemID;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Menu item does not exist';
     END IF;
+END;
 
 --checking if reservation is overbooked
 
