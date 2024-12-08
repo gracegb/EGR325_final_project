074643cb81bc07d8b61cf91f422bc648382b7578
Anquera <155914502+Anquera@users.noreply.github.com>
2024-12-08 15:00:57 -0800

    Update CallProcedures.sql

--- CallProcedures.sql
+++ CallProcedures.sql
@@ -1,1 +1,37 @@
 
+USE restaurant;
+
+-- View Menu
+SELECT * FROM Menu;
+
+-- Menu Procedures
+CALL DeleteMenu('Drink');
+CALL AddMenu(1, 'Drink', 'Non-Alcholic Drinks', '11:30' , '21:30');
+CALL UpdateMenuTimeWindow (1, time, time);
+
+-- Menu Content Procedures
+CALL AddItemToMenu(7, 35);
+CALL RemoveItemFromMenu(7,35);
+
+--  Menu Item Procedures
+CALL AddMenuItem ('Calamari', 12.00, 'Fried Octopus');
+CALL DeleteMenuItem()
+CALL UpdateMenuItemPrice()
+
+-- Menu Item Recipe/Ingredients Procedures
+CALL AddIngredient();
+CALL DeleteIngredient();
+CALL UpdateIngredientQuantityUnit();
+
+-- Inventory Item Procedures
+CALL AddInventoryItem ();
+CALL DeleteInventoryItem();
+CALL UpdateInventoryItemQuantity();
+
+-- Supplier Procedures
+CALL AddSupplier();
+CALL DeleteSupplier();
+CALL UpdateSupplierInformation();
+
+
+
